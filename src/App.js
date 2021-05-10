fcf6f208f2388ef11649354706d5e029118165bd
Jessica <you@example.com>
2021-05-10 18:38:39 +0200

    added project ideas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Component, Fragment} from 'react';
 import Navbar from './components/Navbar';
 import Project from './components/Project'
-import Footer from './components/Footer'
+import Inprogress from './components/Inprogress'
     
     class App extends Component {
       state = {
-        projects:[]
+        projects:[],
+        inprogress:[]
       }
 
       //when the compotent is mounted, fetches data from API and copies JSON into state as projects
@@ -16,16 +17,30 @@ import Footer from './components/Footer'
           this.setState({projects : data})
         })
         .catch(console.log)
+
+
+        fetch('https://djaaango.herokuapp.com/inprogress/')
+        .then(res => res.json())
+        .then((data)=>{
+          this.setState({inprogress : data})
+        })
+        .catch(console.log)
       }
       render () {
         return (
           <Fragment>
           <Navbar></Navbar>
           <a href="https://djaaango.herokuapp.com/">Link to fetched API</a>
-          <Project projects={this.state.projects} />
+          <div className="row">
+            <div className = 'col'>
+              <Project projects={this.state.projects} />
+            </div>
+            <div className = 'col'>
+            <Inprogress inprogress={this.state.inprogress}></Inprogress>
+            </div>
+          </div>
           </Fragment>
         )
       }
     }
-    
     export default App
---
+++ src/components/Inprogress.js
@@ -0,0 +1,21 @@
+import  React,{Fragment} from 'react'
+import Navbar from './Navbar'
+
+const Inprogress = ({ inprogress }) => {
+    return(
+        <div>
+            <h2>In progress and project ideas</h2>
+          {inprogress.map((el) => (
+              <Fragment>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{el.name}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">{el.description}</h6>
+              </div>
+            </div>
+            </Fragment>
+          ))}
+        </div>
+    )
+}
+export default Inprogress
